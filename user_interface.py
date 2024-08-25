df3c9c3c31b97711ced117d28fa1904651e1a599
Radman <radikahn@gmail.com>
2024-08-24 19:44:29 -0700

    added console input for user *temporary solution*

--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,31 @@
+from matplotlib.dates import YearLocator
 from f1collect import *
 
 class user_interface:
-    print(f1collect.driver1_graph(2020, 2, 'FP1', 'LEC'))
+    import traceback
+    import logging
+    from datetime import datetime
+
+    year: int
+    race_number: int
+    race_type: str
+    driver: str
+    current_year = datetime.now().year
+
+
+    while True:
+        try:
+            year = int(input("Enter season Year (Ex = 2019)"))
+            race_number = int(input("Number of race in the year"))
+            race_type = input("Enter event (FP1 = free prac 1, Q1 = qualifying 1)")
+            driver = input("First 3 letters of driver's last name (Ex: Hamilton = HAM)")
+
+
+            print(f1collect.driver1_graph(year, race_number, race_type, driver))
+
+        except Exception as e:
+            if year > current_year:
+                    print("Year is not possible")
+            else:
+                logging.error(traceback.format_exc())
+    
