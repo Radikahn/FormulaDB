922e6d5f2c10ec038e66a9100be96fcda987cce8
Radman <radikahn@gmail.com>
2024-08-24 19:16:48 -0700

    added data recall and graphing

--- f1collect.py
+++ f1collect.py
@@ -5,24 +5,32 @@ class f1collect:
     import requests
    
     @staticmethod
-    def driver1(year: int, track: character, race_type: character, driver: character):
+    def driver1_graph(year: int, race_number: int, race_type: str, driver: str):
+        from matplotlib import pyplot as plt
         import fastf1
+        import fastf1.plotting
         import json
 
-        session = fastf1.get_session(year, track, race_type)
-        session.load(telemetry=False, laps = False, weather = False)
-        driver = session.get_driver(driver)
-        return "Pronto {driver['FirstName']}?"
+        session = fastf1.get_session(year, race_number, race_type)
+        session.load()
+        
 
-    @staticmethod
-    def driver2(year: int, track: character, race_type: character, driver: character):
-        import fastf1
-        import json
+        session.load()
+        fast_driver = session.laps.pick_driver(driver).pick_fastest()
+        driver_car_data = fast_driver.get_car_data()
+        t = driver_car_data['Time']
+        vCar = driver_car_data['Speed']
 
-        session = fastf1.get_session(year, track, race_type)
-        session.load(telemetry=False, laps = False, weather = False)
-        driver = session.get_driver(driver)
-        return "Pronto {driver['FirstName']}?"
+        
+        fig, ax = plt.subplots()
+        ax.plot(t, vCar, label='Fast')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Speed [Km/h]')
+        ax.set_title(f"{driver} speed in race {race_number} of {year}")
+        ax.legend()
+        plt.show()
+        
+
 
     
 
--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,4 @@
 from f1collect import *
 
 class user_interface:
-    print(f1collect.driver1(2019, 'Monza', 'Q', 'HAM'))
+    print(f1collect.driver1_graph(2020, 2, 'FP1', 'LEC'))
